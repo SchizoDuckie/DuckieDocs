62c2afbe706a0c210ce8280486852f8127ae4cd7
jur <jelle@samson-it.nl>
2015-03-17 08:51:26 +0100

    polish

--- js/controllers/HomeCtrl.js
+++ js/controllers/HomeCtrl.js
@@ -30,22 +30,35 @@ DuckieDocs.controller('HomeCtrl', ["Security", "$scope",
             }
         }];
 
+        this.updateDocumentList = function() { 
+            CRUD.Find('Document').then(function(documents) {
+                self.documents = documents;
+                $scope.$applyAsync();
+            })
+        }
+
         this.upload = function() {
             var input = document.querySelector('input[type=file]');
             var files = input.files;
             this.uploadModel.filepath = files[0].path;
+            if(!this.uploadModel.name) {
+                this.uploadModel.name = files[0].name;
+            }
+
             this.uploadModel.Persist().then(function() {
                 console.log('done!', self);
                 self.saved = true;
                 $scope.$applyAsync();
+
+                self.uploadModel = new Document();
+                self.updateDocumentList();
             });
 
         }
 
-        CRUD.Find('Document').then(function(documents) {
-            self.documents = documents;
-            $scope.$applyAsync();
-        })
+        this.updateDocumentList();
+
+        
 
     }
 ])
